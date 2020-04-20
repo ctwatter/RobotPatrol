07fe20b22f642176b3c168e7126fae2e4df4a984
ctwatterson <38928567+ctwatterson@users.noreply.github.com>
2020-04-19 18:07:01 -0700

    asd

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+// Implement a simultaneous two-player mode (50)
+// Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (50)
+// Implement parallax scrolling (15)
+// Create a new title screen (15) (sorta, just a new background pic idk)
+// Allow the player to control the Rocket after it's fired (10)
 let config = {
     type: Phaser.CANVAS,
     width: 640,
